4b7428503878412fe3040219fff0d69768c01965
joshua-cloud-dot <joshua.heninger@posteo.de>
2025-03-01 17:56:52 +0100

    deleted some unnecessary folders and files + moved code from original index file into app and component

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -3,8 +3,13 @@ import CleaningTable from './components/CleaningTable.vue';
 </script>
 
 <template>
+  <header>
+    <h1 class="text-3xl underline">WehGeh Putzplan</h1> 
+  </header>
   <main>
+    <!-- cleaning Table component -->
     <CleaningTable />
   </main>
+  <footer>Example Footer</footer>
 </template>
 
