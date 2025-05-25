a21a894923d11d1477a5bb3850bcfa4a7dbbd0be
Joshua-cloud-dot <joshua.heninger@posteo.de>
2025-05-25 13:24:42 +0200

    Add Impressum so I won't have to pay a bunch of money

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,16 +1,27 @@
 <script setup>
 // import CleaningTable from './components/CleaningTable.vue';
 import NavigationBar from './components/NavigationBar.vue';
+
 </script>
 
 <template>
+  <div class="wrapper">
   <header>
     <NavigationBar/>
   </header>
   <main>
     <router-view></router-view>
   </main>
-  <footer></footer>
+  <footer>
+  <section id="impressum">
+    <h2>Impressum</h2>
+    <p><strong>Name:</strong> Joshua Heninger</p>
+    <p><strong>Anschrift:</strong> Schweizer Strasse 8, 01069 Dresden, Deutschland</p>
+    <p><strong>E-Mail:</strong> <a href="mailto:mail.test@yourmap.com">joshua.heninger@posteo.de</a></p>
+    <p><strong>Inhaltlich verantwortlich gemäß § 55 Abs. 2 RStV: </strong>Joshua Heninger</p>
+  </section>
+</footer>
+</div>
 </template>
 
 
@@ -19,5 +30,22 @@ import NavigationBar from './components/NavigationBar.vue';
   --main-font: "Rethink Sans"
 }
 
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+}
+
+
+footer {
+  font-size: 0.5em;
+  padding: 20px;
+  text-align: left;
+  border-top: 1px solid #ccc;
+}
 
 </style>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,10 +1,12 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import OneSignalVuePlugin from '@onesignal/onesignal-vue3'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import CleaningTable from './components/CleaningTable.vue';
 import Home from './components/Home.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -17,5 +19,13 @@ const router = createRouter({
 
 const app = createApp(App);
 app.use(router);
+app.use(OneSignalVuePlugin, {
+        appId: "222c4fa7-6611-43d0-b5c9-8aebe22680e8",
+      safari_web_id: "web.onesignal.auto.0e731bf1-0f8d-4c8c-8593-03e4c907000a",
+      notifyButton: {
+        enable: true,
+      },
+      subdomainName: "wehgehweb",
+  });
 app.mount('#app');
 
